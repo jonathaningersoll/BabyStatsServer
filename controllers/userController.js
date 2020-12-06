8e2d759c6f1f101405c5d6601783f0cfbbae950e
Jonathan Ingersoll <jonathan.ingersoll@gmail.com>
2020-12-06 18:01:20 -0500

    User validation testing with usercontroller console log

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,6 +37,7 @@ userController.post('/login', function(req, res){
 		{where: {username: req.body.user.username}}
 	).then(function(user){
 		var role = user.role;
+		console.log("user controller user: ",user);
 		bcrypt.compare(req.body.user.password, user.passwordhash, function(err, matches){
 			if (matches) {
 				var token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60*60*24});
